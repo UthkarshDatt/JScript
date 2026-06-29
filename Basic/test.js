099eaf5915e44ce782e594f7c08a03ddf60ec4fc
UthkarshDatt <77330424+UthkarshDatt@users.noreply.github.com>
2026-06-29 18:23:34 +0000

    e29062026/2353

--- Basic/test.js
+++ Basic/test.js
@@ -1,1 +1,38 @@
-console.log("Hello world")
+// console.log("Hello world")
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+let accountName="MINDY"
+let UIN = 34333
+let experienceInYears = 3.3
+// console.table([accountName, UIN, experienceInYears])
+
+// //~~~~~~~~~~~~~~~~~~~~~~~~
+// console.log(3+3); console.log(6+3); 
+
+// "use strict"; //Strict mode takes code as per the latest ver of JS
+// //alert(3+3) in Broswer
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+let Weight= "9i" //0 /12 / NaN / null / undefined /true / false
+
+// console.log("The value in Weight is: " + Weight)
+// console.log("The type of Weight is: " + typeof Weight)
+
+let valueInWeight = Number(Weight)
+
+// console.log("The value in valueInWeight is: " + valueInWeight)
+// console.log("The type of valueInWeight is: " + typeof valueInWeight)
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+let lastActivity = 3
+let lastActivityInBool = Boolean(lastActivity)
+// console.log("The value in lastActivityInBool is: " + lastActivityInBool)
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
