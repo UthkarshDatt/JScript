7fad2f6eb7baacedef9ffdcc87b95fefa62845c1
UthkarshDatt <77330424+UthkarshDatt@users.noreply.github.com>
2026-06-29 21:17:14 +0000

    p30062026/0247

--- Basic/test.js
+++ Basic/test.js
@@ -2,7 +2,7 @@
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 let accountName="MINDY"
-let UIN = 34333
+const UIN = 34333
 let experienceInYears = 3.3
 // console.table([accountName, UIN, experienceInYears])
 
@@ -34,5 +34,57 @@ let lastActivityInBool = Boolean(lastActivity)
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// console.log("2"+1)
+// console.log(typeof ("2"+1))
+
+// console.log(2+1)
+// console.log(typeof (2+1))
+
+// console.log(3+"4")
+// console.log(typeof (3+"4"))
+
+// console.log("2"+"1")
+// console.log(typeof ("2"+"1"))
+
+
+// console.log("2">1) //true   
+// console.log("2"<1) //false
+// console.log("a00002"<1) //false
+// console.log("a00002">1) //false
+
+// console.log(null>1) //false
+// console.log(null<1) //true
+// console.log(null==1) //false
+// console.log(null==0) //false
+// console.log(null>=0) //true
+// console.log(null<=0) //true
+//equality operator works differently than relational operators. 
+// Null is converted to 0 for relational operators but not for equality operator. 
+// Hence the difference in results.
+
+// console.log(undefined>1) //false
+// console.log(undefined<1) //false
+// console.log(undefined==1) //false
+// console.log(undefined==0) //false
+// console.log(undefined>=0) //false
+// console.log(undefined<=0) //false
+//undefined is not converted to 0 for relational operators or equality operator. 
+// Hence the difference in results.
+
+// console.log(NaN>1) //false
+// console.log(NaN<1) //false
+// console.log(NaN==1) //false
+// console.log(NaN==0) //false
+// console.log(NaN>=0) //false
+// console.log(NaN<=0) //false
+//NaN is not converted to 0 for relational operators or equality operator. 
+// Hence the difference in results.
+
+
+console.log("2"==2) //true
+console.log("3"==2) //false
+console.log("2"===2) //false
+console.log(2===2) //true // === is strict equality operator. It checks for both value and type.
 
 
